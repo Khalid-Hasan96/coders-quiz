904bd8f85335a924c722d4df6124e0cba66c0c69
Khalid-Hasan96 <khbappy66@gmail.com>
2022-10-11 21:45:19 +0600

    tab update

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -12,24 +12,24 @@ const Header = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link to='/'>Home</Link></li>
-                        <li><Link to='/statistics'>Statistics</Link></li>
-                        <li><Link to='/blog'>Blog</Link></li>
+                        <li><Link className='font-semibold text-lg' to='/'>Home</Link></li>
+                        <li><Link className='font-semibold text-lg' to='/statistics'>Statistics</Link></li>
+                        <li><Link className='font-semibold text-lg' to='/blog'>Blog</Link></li>
                     </ul>
                 </div>
                 <div className='flex items-center'>
                     <img src={logo} className='logo' alt="" />
-                    <Link to='/' className="btn btn-ghost normal-case text-xl title">Coders Quiz</Link></div>
+                    <Link to='/' className="btn btn-ghost normal-case text-3xl font-bold title">Coders Quiz</Link></div>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/statistics'>Statistics</Link></li>
-                    <li><Link to='/blog'>Blog</Link></li>
+                    <li><Link className='font-semibold text-lg' to='/'>Home</Link></li>
+                    <li><Link className='font-semibold text-lg' to='/statistics'>Statistics</Link></li>
+                    <li><Link className='font-semibold text-lg' to='/blog'>Blog</Link></li>
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Get started</a>
+                <Link to='/' className="btn btn-warning text-white font-medium">Get started</Link>
             </div>
         </div>
     );
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const getQuizContainer = useLoaderData();
+    const quizContainer = getQuizContainer.data
     return (
         <div>
-            <h1>Home</h1>
+            {
+                quizContainer.map(quiz => console.log(quiz))
+            }
         </div>
     );
 };
