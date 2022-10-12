f3f00379ceb358db4c0c7f9bb74380c29f7bfaf3
Khalid Hasan <91031263+Khalid-Hasan96@users.noreply.github.com>
2022-10-12 22:18:38 +0600

    quiz name update

--- src/component/Questions/Questions.js
+++ src/component/Questions/Questions.js
@@ -4,9 +4,11 @@ import QuestionAns from '../QuestionAns/QuestionAns';
 const Questions = () => {
     const questions = useLoaderData();
     const questionContainer = questions.data.questions;
+    const topic = questions.data.name;
 
     return (
         <div>
+            <h1 className='mt-12 w-3/4 mx-auto text-center bg-yellow-200 font-bold text-2xl underline py-5 rounded-lg'>Quiz Topic: {topic}</h1>
             {
                 questionContainer.map(question =>
                     <QuestionAns key={question.id} questionAns={question}></QuestionAns>
